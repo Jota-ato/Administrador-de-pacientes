9fea11fbd9e3fe2b9ea48d7c6641feac20ad340b
Jota-ato <julioc-01@outlook.com>
2025-12-16 12:16:32 -0600

    refactor(.\index.html, .\src\App.tsx, .\src\components\PatientForm.tsx, .src\components\PatientsList.tsx): Change colors Change the color of the spans and buttons from amber-600 to indigo-600 using Tailwindcss classes

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -18,7 +18,7 @@ export default function PatientsList() {
                     <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
                     <p className="text-xl mt-5 mb-10 text-center">
                         Administra tus {''}
-                        <span className="text-amber-600 font-bold">Pacientes y citas</span>
+                        <span className="text-indigo-600 font-bold">Pacientes y citas</span>
                     </p>
                     {patients.map( patient => (
                         <PatientDetails
@@ -32,7 +32,7 @@ export default function PatientsList() {
                     <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
                         <p className="text-xl mt-5 mb-10 text-center">
                             Comienza agregando pacientes {''}
-                        <span className="text-amber-600 font-bold">y apareceran en este lugar</span>
+                        <span className="text-indigo-600 font-bold">y apareceran en este lugar</span>
                     </p>
                 </>
             )}
